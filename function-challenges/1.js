2b89bcafe8ab9210ab0be3b94161515856548f6b
Jasenpan1987 <jasenpan@gmail.com>
2018-08-09 00:11:23 +1000

    function challenge part 1

--- function-challenges/1.js
+++ function-challenges/1.js
@@ -81,3 +81,81 @@ function curryMul2(func, ...args1) {
 }
 
 // Q5
+// use the exist functions, create the inc function
+// var inc = _ _ _;
+// inc(5) // 6
+// inc(inc(5)) // 6
+var inc = curry(add, 1);
+var inc2 = liftf(add)(1);
+var inc3 = addf(1);
+
+// Q6
+// write a function twice, that takes a binary function
+// and returns a unary function that passes its argument
+// twice to the binary function
+// add(11, 11) // 22
+// var double = twice(add);
+// double(11) // 22
+function twice(binaryFunc) {
+  return function(arg) {
+    return binaryFunc(arg, arg);
+  };
+}
+
+var doubl = twice(add); // will be used later
+var sqr = twice(mul); // will be used later
+
+// Q7
+// write reverse, a function that reverses the arguments
+// of a binary function
+// var reversedSub = reverse(sub);
+// reversedSub(5, 1) // -4
+function reverse(func) {
+  return function(arg1, arg2) {
+    return func(arg2, arg1);
+  };
+}
+
+function reverse2(func) {
+  return function(...args) {
+    return func(...args.reverse());
+  };
+}
+
+// Q8
+// write a function composeu that takes two unary functions
+// and returns a unary function that call them both
+// var doublAndSqr = composeu(doubl, sqr)
+// doublAndSqr(5) // 100
+function composeu(func1, func2) {
+  return function(arg) {
+    return func2(func1(arg));
+  };
+}
+
+// Q9
+// write a function composeb that takes two binary functions
+// and returns a function that call them both
+// composeb(add, mul)(2, 3, 7) // 35
+function composeb(func1, func2) {
+  return function(arg1, arg2, arg3) {
+    return func2(func1(arg1, arg2), arg3);
+  };
+}
+
+// Q10
+// write a limit function that allows a binary function
+// to be called a limit number of times
+// var addLimit1 = limit(add, 1)
+// addLimit1(1, 2); // 3
+// addLimit1(3, 5); // undefined
+function limit(binaryFunc, times) {
+  var calledCount = 0;
+  return function(arg1, arg2) {
+    calledCount += 1;
+    if (calledCount <= times) {
+      return binaryFunc(arg1, arg2);
+    }
+    return undefined;
+  };
+}
